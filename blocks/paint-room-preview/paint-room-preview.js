a33e179f1ff81c438be0be8755f46777afc1e4fe
Josiah <josiah.huckins@perficient.com>
2025-12-11 16:55:32 -0500

    Ensuring name and ID are in sync.

--- blocks/paint-room-preview/paint-room-preview.js
+++ blocks/paint-room-preview/paint-room-preview.js
@@ -28,7 +28,7 @@ export default async function decorate(block) {
   }
 
   // Pull color data from BYOM
-  const json = await fetchData('devopsdrops.tech/colorapi/colors.json');
+  const json = await fetchData('https://devopsdrops.tech/colorapi/colors.json');
   const colors = json.data;
 
   const PAGE_SIZE = 30;
